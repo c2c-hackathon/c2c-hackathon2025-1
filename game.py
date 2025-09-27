212fdc39bfa79d0d92121e1a63e81c4c05b8f087
c2c-hackathon <hackathon.c2c@gmail.com>
2025-09-27 21:12:11 +0100

    our code

--- game.py
+++ game.py
@@ -17,72 +17,125 @@ USE_LED_HAT = True
 class ButtonInfo:
     color: str
     sound: str
-    matched: bool
-
+    matched: bool = False
 
 class Game:
-    
-    
     def __init__(self, button_pad: MatrixButtonLEDController):
         self.button_pad = button_pad
         self.button_pad.assign_button_events(self.when_pressed, self.when_held, self.when_released)
-        self.buttons: typing.List[ButtonInfo] = []
-        self.sounds: typing.List[str] = []
-        self.colors: typing.List[str] = []
         self.speaker = library.speaker.Speaker()
-        self.initialize_button_pad()
-        self.started = False
         self.play_game = True
         self.queue = queue.Queue()
-        self.numbers1: typing.List[str] = []
-        self.numbers2: typing.List[str] = []
-        self.pairs: typing.List[str] = []
-
-
+        self.selections: typing.List[int] = []
+        self.pairs: typing.List[typing.List[int]] = []
+        self.colors = [
+            "red", "yellow", "orange", "green", "blue", "purple", "white", "brown"
+        ]
+        self.sounds = [
+            "thunder2", "fart_z", "baby_x", "slide_whistle_x",
+            "arrow2", "phone_pay", "bloop_x", "car_horn_x"
+        ]
+        self.initialize_button_pad()
 
     @property
     def correct_sound(self):
-        """The sound that is played when player gets a pair"""
-        # OPTIONAL: change this to a different sound if you want
+        self.speaker.play_preloaded_wav("correct_answer", wait_until_done=True)
         return "correct_answer"
 
     @property
     def incorrect_sound(self):
-        """The sound that is played when player makes an incorrect guess"""
-        # OPTIONAL: change this to a different sound if you want
+        self.speaker.play_preloaded_wav("incorrect", wait_until_done=True)
         return "incorrect"
 
     @property
     def end_of_game_sound(self):
-        """The sound that is played when the game ends."""
-        # OPTIONAL: change this to a different sound if you want
+        self.speaker.play_preloaded_wav("end_of_game", wait_until_done=True)
         return "end_of_game"
 
+    def _get_pair_index(self, button_number: int) -> int:
+        for i in range(len(self.pairs)):
+            if button_number in self.pairs[i]:
+                return i
+        return -1
+
     def _background_logic_checker(self):
+        matched_pairs = 0
+        total_pairs = len(self.pairs)
+
         while self.play_game:
-            time.sleep(0.005)  # Prevents busy-waiting
+            time.sleep(0.005)
             if self.queue.empty():
                 continue
+
             button_number = self.queue.get()
             print(f"Handling button {button_number}")
+            pair_index = self._get_pair_index(button_number)
 
-            # Example logic: light up the button that was pressed with a constant color
-            button = self.button_pad.get_button(button_number)
-            self.button_pad.set_button_led_color(button, "red")
-            self.speaker.play_preloaded_wav("bloop_x", wait_until_done=True)  # Play a sound when button is pressed
-            # TODO: check your game state, and update things
+            if pair_index == -1:
+                continue  
+
+            self.button_pad.set_button_led_color(
+                self.button_pad.get_button(button_number),
+                self.colors[pair_index]
+            )
+            self.speaker.play_preloaded_wav(self.sounds[pair_index], wait_until_done=True)
+
+            self.selections.append(button_number)
+
+            if len(self.selections) == 2:
+                first = self.selections[0]
+                second = self.selections[1]
+                first_idx = self._get_pair_index(first)
+                second_idx = self._get_pair_index(second)
+
+                if first == second:
+                    self.selections = []
+                    continue
+
+                if first_idx == second_idx:
+                    print("Correct Match!")
+                    self.correct_sound
+                    matched_pairs += 1
+                else:
+                    print("Wrong Match!")
+                    self.incorrect_sound
+                    time.sleep(0.5)
+                    self.button_pad.set_button_led_color(self.button_pad.get_button(first), "black")
+                    self.button_pad.set_button_led_color(self.button_pad.get_button(second), "black")
+
+                self.selections = []
+
+                if matched_pairs == total_pairs:
+                    print("Game Over - All pairs matched!")
+                    self.end()
+                    break
+
+    def end(self):
+        colors = ["red", "orange", "yellow", "green", "blue", "purple", "red", "orange",
+                  "yellow", "green", "blue", "purple", "red", "orange", "yellow", "green"]
+        for i in range(1, 17):
+            self.button_pad.set_button_led_color(self.button_pad.get_button(i), colors[i - 1])
+            time.sleep(0.1)
+
+        self.end_of_game_sound()
 
     def when_pressed(self, button):
-        # TODO: this is called when a button is pressed. Add what you need to here
         _logger.info(f"Button {button.pin.info.number} pressed")
         self.queue.put(button.pin.info.number)
 
     def when_held(self, button):
-        # TODO: this is called when a button is held. Add what you need to here
-        pass
+        if button.pin.info.number == 1:
+            self.speaker.play_preloaded_wav("doorbell_x", wait_until_done=True)
+            self.button_pad.clear_button_pad()
+        elif button.pin.info.number == 2:
+            for i in range(8):
+                for j in range(2):
+                    value = self.pairs[i][j]
+                    btn = self.button_pad.get_button(value)
+                    self.speaker.play_preloaded_wav(self.sounds[i], wait_until_done=True)
+                    self.button_pad.set_button_led_color(btn, self.colors[i])
 
     def when_released(self, button):
-        # TODO: this is called when a button is released. Add what you need to here
         pass
 
     def initialize_button_pad(self):
@@ -113,19 +166,6 @@ class Game:
         self.numbers1 = [1,2,3,4,5,6,7,8]
         self.numbers2 = [9,10,11,12,13,14,15,16]
         # TODO: assign to buttons
-
-    def create_pairs(self):
-        i = 0
-        self.numbers1 = [1,2,3,4,5,6,7,8]
-        self.numbers2 = [9,10,11,12,13,14,15,16]
-        for i in range(1,9):
-            v1 = random.choice(self.numbers1)
-            v2 = random.choice(self.numbers2)
-            self.pairs.append([v1,v2])
-            self.numbers1.remove(v1)
-            self.numbers2.remove(v2)
-            print(self.pairs),
-            i = i + 1,
     
     def set_pairs(self):
         
@@ -138,11 +178,24 @@ class Game:
                 self.button_pad.set_button_led_color(button, self.colors[i])
 
     
+
+
+    def create_pairs(self):
+        self.pairs = []
+        numbers1 = [1,2,3,4,5,6,7,8]
+        numbers2 = [9,10,11,12,13,14,15,16]
+        for _ in range(8):
+            v1 = random.choice(numbers1)
+            v2 = random.choice(numbers2)
+            self.pairs.append([v1, v2])
+            numbers1.remove(v1)
+            numbers2.remove(v2)
+        print("Pairs:", self.pairs)
+
     def _start_game(self):
         self.thread = threading.Thread(target=self._background_logic_checker)
         self.thread.start()
-        # TODO: play a sound to start the game
-        self.started = True
+        self.speaker.play_preloaded_wav("arrow2", wait_until_done=True)
 
     def play(self):
         self._start_game()
@@ -155,18 +208,16 @@ class Game:
             self.thread.join()
             self.button_pad.cleanup()
 
-
 def _main():
     button_pad = MatrixButtonLEDController(
         scan_delay=0.020, pwm_freq=10000, display_pause=0.001, use_led_hat=USE_LED_HAT
     )
-    
+
     game = Game(button_pad)
     game.create_pairs()
     game.set_pairs()
     game.play()
     
-
 
 
 if __name__ == "__main__":
